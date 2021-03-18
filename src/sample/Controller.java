8cb5de4754df64d6a485cfabc18d56fe505af05c
cerberus <cerberusprogrammer@gmail.com>
2021-03-18 15:44:46 -0700

    Practica terminada, espero...

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,37 +1,72 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    @FXML
+    private TextArea activity;
+    @FXML
+    private TextField inputSearch;
+
+    int [] aleatorios = new int[20];
+
+    @FXML
+    void generateElements() {
+        for (int i = 0; i < aleatorios.length; i++)
+            aleatorios[i] = (int) (Math.random() * 20);
+    }
+
+    @FXML
+    void showMax() {
+        activity.appendText("El numero maximo es: " + maximo(aleatorios, 0) + "\n");
+    }
+
+    @FXML
+    void showArray() {
+        activity.appendText("Los numeros que hay en el arreglo son: \n" +
+                mostrar(aleatorios, aleatorios.length - 1, 0) + "\n");
+    }
+
+    @FXML
+    void sumPar() {
+        activity.appendText("La suma de los numeros pares son: \n" +
+                sumarPar(aleatorios, aleatorios.length - 1) + "\n");
+    }
+
+    @FXML
+    void showPares() {
+        activity.appendText("Los numeros pares en el arreglo son: \n" +
+                mostrarPar(aleatorios, aleatorios.length - 1) + "\n");
+    }
+
+    @FXML
+    void searchLineal() {
+        if (isNumeric(inputSearch.getText())) {
+            int clave = Integer.parseInt(inputSearch.getText());
+            int index = busquedaLineal(aleatorios, clave, aleatorios.length - 1, 0);
+
+            if (index > 0)
+                activity.appendText("Se ha encontrado el numero, esta en la posicion: " + index + "\n");
+            else
+                activity.appendText("El numero no se encuentra en el arreglo" + "\n");
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ejercicio1();
+        generateElements();
     }
 
-    void ejercicio1 () {
-        int[] a = generarAleatorios();
-
-        System.out.println(maximo(a, 0));
-        System.out.println(sumarPar(a, a.length - 1));
-        System.out.println("Todos:" + mostrar(a, a.length - 1));
-        System.out.println("Todos par:" + mostrarPar(a, a.length - 1));
-    }
-
-    int[] generarAleatorios() {
-        int [] aleatorios = new int[20];
-
-        for (int i = 0; i < aleatorios.length; i++)
-            aleatorios[i] = (int) (Math.random() * 20);
-
-        return aleatorios;
-    }
-
-    static int maximo (int t[], int pos) {
+    int maximo (int[] t, int pos) {
         int res;
 
         if(pos == t.length - 1)
@@ -48,7 +83,7 @@ public class Controller implements Initializable {
         return(res);
     }
 
-    public static int sumarPar(int[] t, int n) {
+    int sumarPar(int[] t, int n) {
         if (n == 0)
             return 0;
         if (t[n] % 2 == 0)
@@ -57,14 +92,14 @@ public class Controller implements Initializable {
             return sumarPar(t, n - 1);
     }
 
-    static String mostrar(int t[], int n) {
-        if (n == 0)
-            return " " + t[n];
+    String mostrar(int[] t, int size, int i) {
+        if (i == size)
+            return " " + t[i];
         else
-            return " " + t[n] + mostrar(t, n - 1);
+            return " " + t[i] + mostrar(t, size, i + 1);
     }
 
-    static String mostrarPar(int t[], int n) {
+    String mostrarPar(int[] t, int n) {
         if (n == 0)
             return "";
         if (t[n] % 2 == 0)
@@ -73,7 +108,7 @@ public class Controller implements Initializable {
             return mostrarPar(t, n - 1);
     }
 
-    int busquedaLineal (int A[], int clave, int n, int i) {
+    int busquedaLineal (int[] A, int clave, int n, int i) {
         if (i == n + 1)
             return -1;
         else if (A[i] == clave)
@@ -82,14 +117,12 @@ public class Controller implements Initializable {
             return busquedaLineal(A, clave, n, i + 1);
     }
 
-
-    /*
-    *
-    * Llene de forma aleatoria un arreglo de 20 elementos numéricos y realice las siguiente acciones.
-    1.Obtener el elemento máximo del arreglo.
-    2.Sumar los números pares del arreglo
-    3.Mostrar el arreglo.
-    4.Mostrar los números pares del arreglo.
-    5.Realice la búsqueda lineal.
-    * */
+    boolean isNumeric(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Recursive-fx");
+        primaryStage.setScene(new Scene(root, 600, 770));
         primaryStage.show();
     }
 
